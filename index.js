d2328acb88604c8b13500f73106f79f18abb5017
Sasha867 <sashacordoba@mail.ru>
2022-06-17 21:02:51 +0300

    Add/words

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const words = [
+    'truck',
+    'pencil',
+    'rain',
+    'banana',
+    'potato',
+    'car',
+    'mountain',
+    'bike',
+    'house',
+    'elephant'
+]
+
 function generateWordRandom(words){
     const indexWord = Math.floor(Math.random() * (10 - 1)) + 1;
     console.log (words[indexWord]);

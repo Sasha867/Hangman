edbef3707c7e925114f83819982a181f8b40a17e
Sasha867 <sashacordoba@mail.ru>
2022-06-20 20:18:41 +0300

    add func

--- createElements.js
+++ createElements.js
@@ -1,15 +1,14 @@
-export {createLetters}
+export { createLetters };
+import { word } from "./index.js";
 
 const wordContainer = document.getElementById('containerWordLine');
 
-function createLetters(){
-    const wordContainer = document.getElementById('containerWordLine');
-    // const arrWord = secretWord.split('');
-    word.forEach((item)=>{
-        const span = document.createElement('span');
-        wordContainer.append(span);
-        // span.textContent = item;
-    })
-}
+function createLetters() {
+  const wordContainer = document.getElementById("containerWordLine");
+  word.forEach((item) => {
+    const span = document.createElement("span");
+    wordContainer.append(span);
+    // span.textContent = item;
+  });
 
-createLetters()
+}

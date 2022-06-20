eec051e5b2d28477ae29d47b50c936123d689553
Sasha867 <sashacordoba@mail.ru>
2022-06-20 19:04:24 +0300

    add func randomWords

---
+++ utils.js
@@ -0,0 +1,10 @@
+export { randomWords };
+
+function mathRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randomWords(list) {
+  let word = list[mathRandom(list.length)];
+  return word.split("");
+}
